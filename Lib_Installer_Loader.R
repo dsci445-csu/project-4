d9d01ea02614ec07c7e92a176ec383d2c8abbe09
HendersonAR <135654274+HendersonAR@users.noreply.github.com>
2024-12-02 16:42:05 -0700

    Create Lib_Installer_Loader.R
    Script responsible for installing required libraries

---
+++ Lib_Installer_Loader.R
@@ -0,0 +1,19 @@
+# Package names
+packages <- c("tidyverse","tidymodels","sf",
+              "ggplot2", "ggmap", "RColorBrewer",
+              "patchwork","fields","MASS",
+              "dbscan","here","kknn",
+              "xgboost","readr","forecast",
+              "forecastHybrid","fpp2","TTR",
+              "dplyr","lubridate","plotly",
+              "rio","tseries","zoo","keras",
+              "tensorflow")
+              
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
